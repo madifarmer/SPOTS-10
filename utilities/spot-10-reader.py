e1ae46fd9789ab13c26d8a7bd8d05f7fc8d602ab
John Atanbori <36032013+Amotica@users.noreply.github.com>
2024-06-21 21:57:59 +0100

    Update spot-10-reader.py

--- utilities/spot-10-reader.py
+++ utilities/spot-10-reader.py
@@ -4,14 +4,12 @@ import struct
 
 
 class SPOT10Loader:
-    def __init__(self, dataset_dir="."):
-        self.dataset_dir = dataset_dir
-        self.file_train_images = "train-images-idx3-ubyte.gz"
-        self.file_train_labels = "train-labels-idx1-ubyte.gz"
-        self.file_test_images = "t10k-images-idx3-ubyte.gz"
-        self.file_test_labels = "t10k-labels-idx1-ubyte.gz"
 
-    def load_labels_idx(self, filename):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def load_labels_idx(filename):
         try:
             with gzip.open(filename, 'rb') as f:
                 # Read magic number, number of items
@@ -30,8 +28,8 @@ class SPOT10Loader:
         except Exception as e:
             raise RuntimeError(f"Error loading file '{filename}': {str(e)}")
 
-
-    def load_images_idx(self, filename):
+    @staticmethod
+    def load_images_idx(filename):
         try:
             with gzip.open(filename, 'rb') as f:
                 # Read magic number, number of images, rows, and columns
@@ -51,22 +49,23 @@ class SPOT10Loader:
         except Exception as e:
             raise RuntimeError(f"Error loading file '{filename}': {str(e)}")
 
-    def get_data(self, dataset="train"):
-        dataset = dataset.lower()
-        if dataset == "train":
-            images = self.load_images_idx(self.dataset_dir + "/" + self.file_train_images)
-            labels = self.load_labels_idx(self.dataset_dir + "/" + self.file_train_labels)
+    @staticmethod
+    def get_data(dataset_dir="../dataset", kind="train"):
+        kind = kind.lower()
+        if kind == "train":
+            images = SPOT10Loader.load_images_idx(dataset_dir + "/train-images-idx3-ubyte.gz")
+            labels = SPOT10Loader.load_labels_idx(dataset_dir + "/train-labels-idx1-ubyte.gz")
             return images, labels
-        elif dataset == "test":
-            images = self.load_images_idx(self.dataset_dir + "/" + self.file_test_images)
-            labels = self.load_labels_idx(self.dataset_dir + "/" + self.file_test_labels)
+        elif kind == "test":
+            images = SPOT10Loader.load_images_idx(dataset_dir + "/t10k-images-idx3-ubyte.gz")
+            labels = SPOT10Loader.load_labels_idx(dataset_dir + "/t10k-labels-idx1-ubyte.gz")
             return images, labels
         else:
-            raise ValueError(f"Dataset type '{dataset}' not supported. Use 'train' or 'test'.")
+            raise ValueError(f"Dataset type '{kind}' not supported. Use 'train' or 'test'.")
 
 
 # Example implementation
 if __name__ == "__main__":
-    data_loader = SPOT10Loader(dataset_dir="../dataset")
-    data, labels = data_loader.get_data(dataset="Train")
+    data_loader = SPOT10Loader()
+    data, labels = data_loader.get_data(kind="train")
     print(data.shape, labels.shape)
