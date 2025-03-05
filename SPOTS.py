ba87e4d274e8f13d364922b6c8553211272422fd
madifarmer <madifarmer@outlook.com>
2025-03-05 15:07:17 +0000

    Create SPOTS.py

---
+++ SPOTS.py
@@ -0,0 +1,7 @@
+import os
+
+print(os.getcwd())  # Prints the current directory
+os.chdir("C:/Users/madif/Dropbox/My PC (LAPTOP-BSA4K39R)/Documents/Masters/MTHM602/SPOTS-10")  # Change the current directory
+print(os.getcwd())  # Prints the current directory
+
+git clone git@github.com:Amotica/SPOTS-10.git 
